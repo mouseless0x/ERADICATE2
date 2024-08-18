2a983b6ed963258ca249c69af885a178e97ce23e
mouseless <97399882+mouseless-eth@users.noreply.github.com>
2024-08-18 02:53:53 +0100

    undo

--- mine-v6.sh
+++ mine-v6.sh
@@ -1,1 +1,1 @@
-./ERADICATE2.x64 -A 0x4e59b44847b379578588920ca78fbf26c0b4956c -i init-code-v6 --leading 0 | nc autorack.proxy.rlwy.net 28701
+./ERADICATE2.x64 -A 0x4e59b44847b379578588920ca78fbf26c0b4956c -i init-code-v6 --leading 0
